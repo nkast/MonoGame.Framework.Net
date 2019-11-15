fade55a93a4728001de202a93f73ec967d2d077f
Nikos Kastellanos <nkastellanos@gmail.com>
2019-11-15 18:32:12 +0200

    add Xna.Framework.Net files

---
+++ MonoGame.Framework.Net/Net/Utilities/DebugHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Microsoft.Xna.Framework.Net
+{
+    /// <summary>
+    /// Provides helper methods to make it easier
+    /// to safely raise events.
+    /// </summary>
+    internal static class DebugHelpers
+    {
+        [System.Diagnostics.Conditional("DEBUG")]
+        internal static void Log(string message)
+        {
+#if !WP8
+            Game.Instance.Log(message);
+#endif
+        }
+    }
+}
